92d3676c4ba43fcc055e4390114ea7735b13f6ca
Ahmadalhomsi <tarikuka.aa@gmail.com>
2024-05-02 12:07:04 +0300

    Mp3 downloading algorithm

---
+++ app/api/yt/route.js
@@ -0,0 +1,62 @@
+import { NextResponse } from "next/server";
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+
+
+
+
+
+export async function POST(request, response) {
+    try {
+        const data = await request.json();
+        const url = data.url;
+        const type = data.type;
+
+        // Regular expression to match YouTube video URLs
+        const youtubeUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[^\s]+$/;
+
+        // Validate the YouTube URL
+        if (!youtubeUrlRegex.test(url)) {
+            return NextResponse.json({ error: "Invalid YouTube link. Please enter a valid YouTube video URL." }, { status: 400 });
+        }
+
+
+
+
+
+
+        const fileName = `video.${type === 'mp3' ? 'mp3' : 'mp4'}`;
+
+        if (type === "mp3") {
+            const videoStream = ytdl(url, {
+                format: type,
+                filter: type === 'mp3' ? 'audioonly' : 'videoonly',
+            });
+            // Set headers for file download
+            const headers = {
+                "Content-Disposition": `attachment; filename="${fileName}"`,
+                "Content-type": `audio/mp3`,
+            };
+
+            // Return the response with headers and streaming data
+            return new Response(videoStream, { headers });
+        }
+        else if (type === "mp4") {
+            const videoStream = ytdl(url);
+            // Set headers for file download
+            const headers = {
+                "Content-Disposition": `attachment; filename="${fileName}"`,
+                "Content-type": `video/mp4`,
+            };
+
+            // Return the response with headers and streaming data
+            return new Response(videoStream, { headers });
+        }
+
+
+
+    } catch (err) {
+        console.error("Error:", err);
+        return NextResponse.json({ error: "Internal server error " + err }, { status: 500 });
+    }
+}
