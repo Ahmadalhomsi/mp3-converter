cc5b05e22cbace0c11ab8fa7358c73a3fcc06278
Ahmadalhomsi <tarikuka.aa@gmail.com>
2024-05-08 00:04:30 +0300

    Fixing MP4 Download

--- app/api/yt/route.ts
+++ app/api/yt/route.ts
@@ -1,16 +1,22 @@
 import { NextResponse } from "next/server";
 const ytdl = require("ytdl-core");
 const fs = require("fs");
+import ytdl2 from "youtubedl-core";
 
 
 
-
-
-export async function POST(request : Request, response : Response) {
+export async function POST(request: Request, response: Response) {
     try {
         const data = await request.json();
         const url = data.url;
         const type = data.type;
+
+
+        if (!ytdl2.validateURL(url))
+            return NextResponse.json({ error: "Invalid YouTube link. Please enter a valid YouTube video URL." }, { status: 400 });
+
+
+
 
         // Regular expression to match YouTube video URLs
         const youtubeUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[^\s]+$/;
@@ -42,15 +48,21 @@ export async function POST(request : Request, response : Response) {
             return new Response(videoStream, { headers });
         }
         else if (type === "mp4") {
-            const videoStream = ytdl(url);
-            // Set headers for file download
+            const EventEmitter = require('events');
+            EventEmitter.defaultMaxListeners = 15; // Adjust the limit as needed
+            const videoStream = ytdl(url, {
+                format: 'mp4',
+                filter: 'videoandaudio',
+                quality: 'highest' 
+            });
+
             const headers = {
                 "Content-Disposition": `attachment; filename="${fileName}"`,
                 "Content-type": `video/mp4`,
             };
 
-            // Return the response with headers and streaming data
             return new Response(videoStream, { headers });
+
         }
 
 
